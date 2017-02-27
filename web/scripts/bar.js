f7dcbe3f5ab23177b625682c7097a5449a79df4a
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-02-27 16:35:44 +0000

    added transortation icons and started to develop the api script

--- web/scripts/bar.js
+++ web/scripts/bar.js
@@ -1,5 +1,10 @@
 /***** Variables *****/
 var timer;
+var directionsService;
+var directionsDisplay;
+var map;
+var service;
+var loc = {lat:38.73727772078954,lng:-9.303122609853745};
 
 /***** Function declarations *****/
 function openBrowser(link) {
@@ -65,4 +70,68 @@ $(document).ready(function(){
     	var bannerText = $(this).siblings('.orderName').text();
     	showBanner(bannerText);
 	});
+
+	//Activating public transport submenu in "information" overlay
+	$("#publicTransportButton").click(function() {
+		$(".publictransportSubButton").toggle(".visible").css("display", "inline-block");
+	});
+
+
+
+	//Activates the Google Maps searches for the Public Transport submenu
+	$("#subwayButton").click(function() {
+		//travel("amadora este estação de metro", 'WALKING');
+		console.log("SUBWAY");
+
+		var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+		var request = { location: pyrmont, radius: '500', query: 'restaurant' };
+		service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, callback);
+
+
+	});
+	$("#busButton").click(function() {
+		travel("amadora este estação de metro", 'WALKING');
+		console.log("BUS");
+	});
+	$("#trainButton").click(function() {
+		console.log($(this).children(".subMenuTitle").text());
+	});
 });
+
+
+var service;
+
+
+/****** Google Maps API interface *******/
+function initMap() {
+	directionsService = new google.maps.DirectionsService;
+	directionsDisplay = new google.maps.DirectionsRenderer;
+	var map = new google.maps.Map(document.getElementById("mapGoogle"), { center: loc, zoom: 13, disableDefaultUI: true});
+	directionsDisplay.setMap(map);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+	  console.log(place);
+	  createMarker(results[i]);
+    }
+  }
+}
+
+function calculateAndDisplayRoute(dest, tMode) {
+	directionsService.route({
+		origin: loc,
+		destination: dest,
+		travelMode: tMode
+	}, function(response, status) {
+		if (status === 'OK') directionsDisplay.setDirections(response);
+		else window.alert('Directions request failed due to ' + status);
+	});
+}
+
+function travel(dest, travelMode) { calculateAndDisplayRoute(dest, 'WALKING'); }
+function nearby() { }
+function nearbySearch(keyWords) { }
