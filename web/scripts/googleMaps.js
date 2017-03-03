e7e9c7d8f549b7c330cc6a2c0627081403cba043
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-03-03 17:05:56 +0000

    added contigency for google maps directions

--- web/scripts/googleMaps.js
+++ web/scripts/googleMaps.js
@@ -5,10 +5,12 @@ var googleMapsUnits = "&units=metric";
 
 /****** Google Maps code *******/
 function searchMap(keyWords) {
+	if (keyWords == "") return;
 	var url = "https://www.google.com/maps/embed/v1/search"+googleMapsKey+"&q="+keyWords;
 	$("#iframeMap").attr("src", url);
 }
 function directionsMap(destination, travelMode) {
+	if (destination == "") return;
 	var url = "https://www.google.com/maps/embed/v1/directions"+googleMapsKey+googleMapsOrigin+"&destination="+destination+"&mode="+travelMode;
 	$("#iframeMap").attr("src", url);
 }
