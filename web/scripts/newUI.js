41ff162ffa80c149eaed00d4206082be7085e0d4
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-03-06 21:23:12 +0000

    changed the background of home

--- web/scripts/newUI.js
+++ web/scripts/newUI.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
 	$(".orderElementsRow").children("td:contains('+')").addClass("orderIncrement");
 
 	$(".doneIcon").append('<svg fill="#FFFFFF" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>');
-	$(".doneIcon svg").css("fill", "black");
 
 	// Activating arrow toggle
 	$("#arrowIcon").click(function() {
