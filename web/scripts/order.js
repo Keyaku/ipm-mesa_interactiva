c900e377009b0f0e5a2c87664aff112022548367
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-03-23 16:02:40 +0000

    Transformed the html to javascript

--- web/scripts/order.js
+++ web/scripts/order.js
@@ -19,3 +19,22 @@ for (var pizza in pizzaList) {
 	var division = $("<div></div>").addClass("menuPizzaItem").append($("<div></div>").addClass("menuPizzaItemImg").attr("id", "mPII" + (pizzaId++).toString())).append(pizzaItemInfoDiv); //Appends the img division to the main pizza item div.
 	$("#menuPremadePizzas").append(division);
 }
+
+var ingredientsList = {
+	"Cheese"	: ["Mozzarella", "Parmesan", "Provolone"],
+	"Meat"		: ["Ham", "Pepperoni", "Bacon", "Prosciutto", "Chicken", "Beef"],
+	"Fish & Seafood" : ["Tuna", "Sardin", "Shrimp"],
+	"Vegetables" : ["Mushroom", "Jalapenõs", "Tomato", "Peppers"],
+};
+
+for (var ingredientType in ingredientsList) {
+	var typeLabel = $("<label></label>").addClass("pizzaIngredientTypeLabel").append(document.createTextNode(ingredientType)); //Creates the ingredient type label.
+	var typeDiv = $("<div></div>").addClass("pizzaIngredientType"); //Creates the div that contains the all the type's ingredients and their images.
+	for (i in ingredientsList[ingredientType]) {
+		var ingredientDiv = $("<div></div>").addClass("pizzaIngredient"); //Creates a div for each ingredient.
+		var ingredientLabel = $("<label></label>").addClass("pizzaIngredientName").append(document.createTextNode(ingredientsList[ingredientType][i])); //Creates the ingredient's label.
+		ingredientDiv.append($("<div></div>").addClass("pizzaIngredientImg")).append(ingredientLabel); //Appends the ingredient's image div and label to the ingredient's div.
+		typeDiv.append(ingredientDiv); //Appends the individual ingredients div to the types's div.
+	}
+	$("#pizzaIngredients").append(typeLabel).append(typeDiv); //Appends the type's div to the main page.
+}
