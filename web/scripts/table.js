b197efc78aa91fdee99d11875b04adb3cd1fcf2b
Keyaku <antonio.luis.sarmento@gmail.com>
2017-03-23 09:28:45 +0000

    Table: Code cleanup

--- web/scripts/table.js
+++ web/scripts/table.js
@@ -2,7 +2,6 @@
 const ANIM_SPEED_MENU = 500;
 
 /***** Variables *****/
-var timer;
 
 /****** Code execution *******/
 $(document).ready(function(){
@@ -26,25 +25,20 @@ $('#minimizer').click(function() {
 
 //Show the correct overlay
 $('.menuButton').click(function() {
-	$('.overlay').hide(); // Hide ALL submenus, just in case
+	var backgroundImgs = { // Our background "database"
+		'default' : '../img/tableBack2.jpg',
+		'order'   : '../img/menuBannerDrinks.jpg',
+	};
 
-	var b = $('main');
 	var name = $(this).children('.menuTitle').text().toLowerCase();
-
-	switch (name) {
-		case 'order':
-			b.css('background-image', 'url("../img/menuBannerDrinks.jpg")');
-			break;
-		default:
-			b.css('background-image', 'url("../img/tableBack2.jpg")');
+	if (name in backgroundImgs) {
+		$('main').css('background-image', 'url(' + backgroundImgs[name] + ')');
+	} else {
+		$('main').css('background-image', 'url(' + backgroundImgs['default'] + ')');
 	}
 
+	$('.overlay').hide(); // Hide ALL submenus, just in case
 	$('.overlay#' + name).show(); // Show the appropriate submenu
-});
-
-$('.pizzaMakerItem').click(function() {
-	$(this).children('.doneIcon').children().css('fill', 'green');
-	$(this).children('.orderName').css('color', 'white');
 });
 
 //Google Maps search
@@ -55,10 +49,3 @@ $('#publicTransportButton').click(function() { directionsMap($('#mapsDestination
 
 /***** Timer-related code *****/
 // TODO: Add countdown element in page
-
-/***** Ordered elements list -related code *****/
-function showBanner(text) {
-	clearTimeout(timer);
-	$('#orderElementsListBanner').css('background','rgba(255,140,0,1)').css('color','rgba(255,255,255,1)').children('.orderName').text(text);
-	timer = setTimeout(function() { $('#orderElementsListBanner').css('background','rgba(255,140,0,0)').css('color','rgba(255,255,255,0)') }, 3000);
-}
