529b1366b310ca1bb9d9d8df2613198365e2473e
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-03-23 18:14:36 +0000

    finished moddeling the pizza information vertical bar

--- web/scripts/menus/menuPizzaList.js
+++ web/scripts/menus/menuPizzaList.js
@@ -8,11 +8,19 @@ var pizzaList = {
 };
 
 var pizzaNutritionalInfo = {
-	"Pepperoni Lovers" : {"Calories":"120", "Protein":"8", "Carbohydrates":"20", "Fat":"80"},
-	"Four Seasons" : {"Calories":"120", "Protein":"8", "Carbohydrates":"20", "Fat":"80"},
-	"Camponesa" : {"Calories":"120", "Protein":"8", "Carbohydrates":"20", "Fat":"80"},
-	"Camponesa2" : {"Calories":"120", "Protein":"8", "Carbohydrates":"20", "Fat":"80"},
-	"Camponesa3" : {"Calories":"120", "Protein":"8", "Carbohydrates":"20", "Fat":"80"},
+	"Pepperoni Lovers" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+	"Four Seasons" : {"Calories":"190kcal", "Protein":"8g", "Carbohydrates":"56g", "Fat":"999g"},
+	"Camponesa" : {"Calories":"2660kcal", "Protein":"4g", "Carbohydrates":"26g", "Fat":"881g"},
+	"Camponesa2" : {"Calories":"887kcal", "Protein":"1g", "Carbohydrates":"12g", "Fat":"125g"},
+	"Camponesa3" : {"Calories":"556kcal", "Protein":"23g", "Carbohydrates":"44g", "Fat":"99g"},
+}
+
+var pizzaRating = {
+	"Pepperoni Lovers" : "77%",
+	"Four Seasons" : "99%",
+	"Camponesa" : "12%",
+	"Camponesa2" : "9%",
+	"Camponesa3" : "37%",
 }
 
 
@@ -32,5 +40,18 @@ for (var pizza in pizzaList) {
 
 //Generates and shows the extensive information menu of the chosen pizza.
 $(".menuPizzaItem").click(function() {
-	console.log("click");
+	var pizzaName = $(this).children(".menuPizzaItemInfo").children(".mPIITitle").text(); //Gets the pizza's name.
+	var labelName = $("<label></label>").addClass("mPIITitle").append(document.createTextNode(pizzaName)); //Creates the label that represents the pizza's name.
+	//Creates the list of the pizza's ingredients.
+	var list = $("<ul></ul>"); // Creates the list of ingredients.
+	var ingredients = pizzaList[pizzaName];
+	for (i in ingredients) list.append($("<li></li>").addClass("mPIIIngredient").append(document.createTextNode(ingredients[i]))); //Adds each ingredient to the list.
+	//Generates all the nutritional information for the pizza.
+	var nutInfo = pizzaNutritionalInfo[pizzaName];
+	var table = $("<table></table");
+	for (var key in nutInfo) table.append($("<tr></tr>").append($("<td></td>").append(document.createTextNode(key))).append($("<td></td").append(document.createTextNode(nutInfo[key])))); //Creates each table row.
+	//Creates the rating table.
+	var ratingInfo = pizzaRating[pizzaName]; //Gets the pizza's rating.
+	var ratingDiv = $("<div></div").append($("<div></div>").addClass("positiveRatingImg")).append($("<label></label>").append(document.createTextNode(ratingInfo)));
+	$("#pizzaInformation").empty().append(labelName).append(list).append(table).append(ratingDiv);
 });
