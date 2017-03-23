276fcc705670365194a7fb31ea2498afd41b42b7
Keyaku <antonio.luis.sarmento@gmail.com>
2017-03-23 08:44:48 +0000

    Order: Used better Javascript

--- web/scripts/order.js
+++ web/scripts/order.js
@@ -1,17 +1,21 @@
-var pizzasArray = [
-	["Pepperoni Lovers", "Extra Mozarella", "Extra pepperoni"],
-	["Four Seasons", "Ham", "Veggies", "Corn", "Pineapple"],
-	["Camponesa", "Ham", "Onion", "Jalapenõs"],
-	["Camponesa2", "Ham", "Onion", "Jalapenõs"],
-	["Camponesa3", "Ham", "Onion", "Jalapenõs"]
-]
+var pizzaList = {
+	"Pepperoni Lovers" : ["Extra Mozarella", "Extra pepperoni"],
+	"Four Seasons"     : ["Ham", "Veggies", "Corn", "Pineapple"],
+	"Camponesa"        : ["Ham", "Onion", "Jalapenõs"],
+	"Camponesa2"       : ["Ham", "Onion", "Jalapenõs"],
+	"Camponesa3"       : ["Ham", "Onion", "Jalapenõs"],
+};
 
 //Cycles all the pizzas on the menu.
-for (var i = 0; i < pizzasArray.length; i++) {
-	var label = $("<label></label>").addClass("mPIITitle").append(document.createTextNode(pizzasArray[i][0])); //Creates the label that represents the pizza's name.
-	var list = $("<ul></ul>"); //Creates the list of ingredients.
-	for (var j = 1; j < pizzasArray[i].length; j++) list.append($("<li></li>").addClass("mPIIIngredient").append(document.createTextNode(pizzasArray[i][j])));
-	var pizzaItemInfoDiv = $("<div></div>").addClass("menuPizzaItemInfo").attr("id", pizzasArray[i][0]).append(label).append(list); //Adds the pizza's name and the ingredients list to the correct div.
-	var division = $("<div></div>").addClass("menuPizzaItem").append($("<div></div>").addClass("menuPizzaItemImg").attr("id", "mPII" + (i + 1).toString())).append(pizzaItemInfoDiv); //Appends the img division to the main pizza item div.
+var pizzaId = 1;
+for (var pizza in pizzaList) {
+	var label = $("<label></label>").addClass("mPIITitle").append(document.createTextNode(pizza)); //Creates the label that represents the pizza's name.
+	var list = $("<ul></ul>"); // Creates the list of ingredients.
+	for (i in pizzaList[pizza]) {
+		var ingredient = pizzaList[pizza][i];
+		list.append($("<li></li>").addClass("mPIIIngredient").append(document.createTextNode(ingredient)));
+	}
+	var pizzaItemInfoDiv = $("<div></div>").addClass("menuPizzaItemInfo").attr("id", pizza).append(label).append(list); //Adds the pizza's name and the ingredients list to the correct div.
+	var division = $("<div></div>").addClass("menuPizzaItem").append($("<div></div>").addClass("menuPizzaItemImg").attr("id", "mPII" + (pizzaId++).toString())).append(pizzaItemInfoDiv); //Appends the img division to the main pizza item div.
 	$("#menuPremadePizzas").append(division);
 }
