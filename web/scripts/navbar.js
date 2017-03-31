8dd480f18a607646b5282b16354da4b1b55ae10f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-03-31 15:53:07 +0100

    More navbar abstraction
    Now able to add navbar anywhere simple and cleanly.

--- web/scripts/navbar.js
+++ web/scripts/navbar.js
@@ -1,3 +1,27 @@
+let navbar = `<div id="options">
+	<div id="logo"></div>
+	<div class="button">
+		<svg class="icon"><use xlink:href="img/icons/pizza.svg#pizza" /></svg>
+		<label class="title">Order</label>
+	</div>
+	<div class="button">
+		<svg class="icon"><use xlink:href="img/icons/music.svg#music" /></svg>
+		<label class="title">Entertainment</label>
+	</div>
+	<div class="button">
+		<svg class="icon"><use xlink:href="img/icons/world.svg#world" /></svg>
+		<label class="title">News</label>
+	</div>
+	<div class="button">
+		<svg class="icon"><use xlink:href="img/icons/navigation.svg#navigation" /></svg>
+		<label class="title">Information</label>
+	</div>
+</div>
+<div id="minimizer">
+	<svg><use xlink:href="img/icons/arrowLeft.svg#arrowLeft" /></svg>
+</div>`;
+$('nav').html(navbar);
+
 // Activating arrow toggle
 $('nav #minimizer').click(function() {
 	$('nav #options').toggle(ANIM_SPEED_MENU);
