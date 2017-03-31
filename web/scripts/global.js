c5adfbdd9cb7b925e70926de3d86bb585842b469
Keyaku <antonio.luis.sarmento@gmail.com>
2017-03-31 14:55:01 +0100

    Minor fix

--- web/scripts/global.js
+++ web/scripts/global.js
@@ -9,13 +9,12 @@ $(":header").css({cursor: "default"});
 
 /* Functions to attach to elements */
 $.fn.followAlong = function() {
-	var $window = $(window)
     var container = $(this);
     var minTop = container.parent().top;
     var maxTop = container.parent().height() - container.outerHeight(true);
 
     $(document).scroll(function() {
-        if ($window.scrollTop() > maxTop) {
+        if ($(window).scrollTop() > maxTop) {
 			container.css({
                 position: 'absolute',
                 top: maxTop
