2044da3649c65fd07d03dbfd0690637abea97d43
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-04-03 18:49:47 +0100

    wrote the initial barebones code for the drinks menu

--- web/scripts/menus/menuDrinks.js
+++ web/scripts/menus/menuDrinks.js
@@ -4,18 +4,24 @@
 
 ------------------------------------------------------------------------------*/
 //Array of premade pizzas (for the JS generated menu).
-var pizzaList = {
-	"Water" : {"Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"},
-	"Sparkling Water" : {"Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"},
-	"Fresh Juice" : {"Orange Juice (25cl)", "Blueberry Juice (50cl)", "Lemonade (1L)", "Tutti Frutti (50cl)"},
-	"Coca-Cola Original" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
-	"Coca-Cola Zero" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
-	"Coca-Cola" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
-	"Sumol (Orange)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
-	"Sumol (Pineapple)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
-	"Sumol (Passion Fruit)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
-	"Sumol (Mango)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+var drinksList = {
+	"Water" : ["Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"],
+	"Sparkling Water" : ["Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"],
+	"Fresh Juice" : ["Orange Juice (25cl)", "Blueberry Juice (50cl)", "Lemonade (1L)", "Tutti Frutti (50cl)"],
 
+	"Coca-Cola Original" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Coca-Cola Zero" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Coca-Cola" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+
+	"Sumol (Orange)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Sumol (Pineapple)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Sumol (Passion Fruit)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Sumol (Mango)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+
+	"Lipton Ice Tea (Orange)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Lipton Ice Tea (Pineapple)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Lipton Ice Tea (Passion Fruit)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
+	"Lipton Ice Tea (Mango)" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 };
 
 
@@ -24,7 +30,12 @@ var pizzaList = {
 				CODE EXECUTION
 
 ------------------------------------------------------------------------------*/
+$("nav").navbar(); // Adding navigation bar
+$("#drinksInformation").hide(); //Hides the lateral pizza information bar.
 
+//Creates all the elements in the premade pizzas menu.
+var drinkId = 1;
+for (drink in drinksList) { $("#menuDrinks").append(getDrinkItem(drink)); }
 
 
 /*------------------------------------------------------------------------------
@@ -32,10 +43,25 @@ var pizzaList = {
 				AUXILIAR FUNCTIONS
 
 ------------------------------------------------------------------------------*/
-
+function getDrinkImg(name) { return $("<div></div>").addClass("menuDrinkItemImg").attr("id", "mDII" + (drinkId++).toString()); }
+function getDrinkTypeImg(name, index) { return $("<div></div>").addClass("drinksInformationTypeImg").attr("id", "mDII" + (drinkId++).toString() + (index).toString()); }
+function getDrinkItem(name) { return $("<div></div>").addClass("menuDrinkItem").append(getDrinkImg(name)).append($("<label></label>").addClass("mPDITitle").append(document.createTextNode(name))); }
+function getDrinkTypes(name) {
+	var d = $("<div></div");
+	for (i in drinksList[name]) d.append($("<div></div>").addClass().append(getDrinkTypeImg(name, i)).append($("<label></label>").addClass("drinksInformationTypeLabel").append(document.createTextNode(drinksList[name][i]))));
+	return d;
+}
+function showDrinkExtensiveInformation(name){
+	var closeX = $("<div></div>").attr("id", "drinksInformationClose").append(document.createTextNode("X")).click(function(){ hideDrinkExtensiveInformation(); });
+	$("#drinksInformation").show().empty().append(closeX).append(getDrinkTypes(name)); //Unhides, clears and appends the new information to the lateral pizza information bar.
+}
+function hideDrinkExtensiveInformation() { $("#drinksInformation").hide(); }
+function confirmCancel() { /*TODO - FranciscoKloganB: Popup that asks for confirmation for cancelling the order.*/ }
 
 /*------------------------------------------------------------------------------
 
 				MENU FLOW
 
 ------------------------------------------------------------------------------*/
+//Generates and shows the lateral pizza information bar of the chosen pizza.
+$(".menuDrinkItem").click(function() { showDrinkExtensiveInformation($(this).children(".mPDITitle").text()); });
