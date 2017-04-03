2be8cbe5c40bd9324b5fcfe82ebfc75785867825
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-04-03 16:35:02 +0100

    started modeling the drinks menu

---
+++ web/scripts/menus/menuDrinks.js
@@ -0,0 +1,41 @@
+/*------------------------------------------------------------------------------
+
+				DATA STRUCTURES
+
+------------------------------------------------------------------------------*/
+//Array of premade pizzas (for the JS generated menu).
+var pizzaList = {
+	"Water" : {"Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"},
+	"Sparkling Water" : {"Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"},
+	"Fresh Juice" : {"Orange Juice (25cl)", "Blueberry Juice (50cl)", "Lemonade (1L)", "Tutti Frutti (50cl)"},
+	"Coca-Cola Original" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+	"Coca-Cola Zero" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+	"Coca-Cola" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+	"Sumol (Orange)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+	"Sumol (Pineapple)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+	"Sumol (Passion Fruit)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+	"Sumol (Mango)" : {"Glass (25cl)", "Can (33cl)", "Glass (40cl)"},
+
+};
+
+
+/*------------------------------------------------------------------------------
+
+				CODE EXECUTION
+
+------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------
+
+				AUXILIAR FUNCTIONS
+
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+
+				MENU FLOW
+
+------------------------------------------------------------------------------*/
