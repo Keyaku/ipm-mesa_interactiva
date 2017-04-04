36224a5d105b6e3fd443f891fa240721f0a59082
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-04-04 16:32:55 +0100

    fixed spawning of images in the drinks menu

--- web/scripts/menus/menuDrinks.js
+++ web/scripts/menus/menuDrinks.js
@@ -3,67 +3,75 @@
 				DATA STRUCTURES
 
 ------------------------------------------------------------------------------*/
-//Array of premade pizzas (for the JS generated menu).
+//Array of premade drinks (for the JS generated menu).
 var drinksList = {
 	"Water" : {
 		"Sizes" : ["Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
-		"Img" : "url('img/menus/pizzaMenu/menuPizza1.png')",
+		"Img" : "img/menus/pizzaMenu/menuPizza1.png",
 	},
 	"Sparkling Water" : {
 		"Sizes" : ["Bottle (25cl)", "Bottle (50cl)", "Bottle (1L)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
-		"Img" : "url('img/menus/pizzaMenu/menuPizza2.png')",
+		"Img" : "img/menus/pizzaMenu/menuPizza2.png",
 	},
 	"Fresj Juice" : {
 		"Sizes" : ["Orange Juice (25cl)", "Blueberry Juice (50cl)", "Lemonade (1L)", "Tutti Frutti (50cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
-		"Img" : "url('img/menus/pizzaMenu/menuPizza3.png')",
+		"Img" : "img/menus/pizzaMenu/menuPizza3.png",
 	},
 
 	"Coca-Cola Original" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
-		"Img" : "url('img/menus/pizzaMenu/menuPizza4.png')",
+		"Img" : "img/menus/pizzaMenu/menuPizza4.png",
 	},
 	"Coca-Cola Zero" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
-		"Img" : "url('img/menus/pizzaMenu/menuPizza5.png')",
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 
 	"Sumol (Orange)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 	"Sumol (Pineapple)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 	"Sumol (Passion Fruit)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 	"Sumol (Mango)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 
 	"Lipton Ice Tea (Orange)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 	"Lipton Ice Tea (Pineapple)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 	"Lipton Ice Tea (Passion Fruit)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 	"Lipton Ice Tea (Mango)" : {
 		"Sizes" : ["Glass (25cl)", "Can (33cl)", "Glass (40cl)"],
 		"NutInfo" : {"Calories":"120kcal", "Protein":"8g", "Carbohydrates":"20g", "Fat":"80g"},
+		"Img" : "img/menus/pizzaMenu/menuPizza5.png",
 	},
 };
 
@@ -88,9 +96,14 @@ for (drink in drinksList)
 				AUXILIAR FUNCTIONS
 
 ------------------------------------------------------------------------------*/
+function getDrinkImg(name) {
+	var img = $("<img>").addClass("menuDrinkItemImg"); //Creates the image.
+	img.attr("src", drinksList[name]["Img"]); //Sets the correct source.
+	return img;
+}
 function getDrinkItem(name) {
 	var label = $("<label></label>").addClass("mPDITitle").append(document.createTextNode(name)); //Creates the label for each drink;
-	var img = $("<img>").addClass("menuDrinkItemImg").src(drinksList[name]["Img"]); //Creates the div and sets the background-image to the correct image.
+	var img = getDrinkImg(name); //Gets the drink's image.
 	return $("<div></div>").addClass("menuDrinkItem").append(img).append(label);
 }
 function getDrinkNutritionalInfo (name) {
@@ -117,6 +130,7 @@ function getDrinkOrderButton() {
 	var button = $("<button></button>").addClass("drinksInformationTypeLabel").append(document.createTextNode("Order")); //Creates a button for ordering.
 	return $("<div></div>").append(price).append(button);
 }
+
 function showDrinkExtensiveInformation(name) {
 	$("#drinksInformation").empty().show(); //Shows the lateral pizza information bar.
 	var closeX = $("<div></div>").attr("id", "drinksInformationClose").append(document.createTextNode("X")).click(function(){ hidePizzaExtensiveInformation(); });
@@ -127,7 +141,7 @@ function showDrinkExtensiveInformation(name) {
 	$("#drinksInformation").append(closeX).append(label).append(nutInfo).append(drinkTypes).append(drinkOrder);
 }
 function hidePizzaExtensiveInformation() { $("#drinksInformation").hide(); }
-
+function confirmCancel() { /*TODO - FranciscoKloganB: Popup that asks for confirmation for cancelling the order.*/ }
 
 /*------------------------------------------------------------------------------
 
