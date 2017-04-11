65ac7080049ab0ec382bb6bbaab5b83df9d683dd
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-04-11 07:30:53 +0100

    pushing 4 kiaku

--- web/scripts/menus/menuCustomizePizza.js
+++ web/scripts/menus/menuCustomizePizza.js
@@ -1,3 +1,7 @@
+var smallPizza = $('#size1');
+var mediumPizza = $('#size2');
+var largePizza = $('#size3');
+
 var ingredientsList = {
 	"Cheese"	: ["Mozzarella", "Parmesan", "ElementalCheese", "FrenchCheese", "GoatCheese", "Parmesan", "SourCream" ],
 	"Meat"		: ["Ham", "Pepperoni", "Bacon", "Prosciutto", "Chicken", "Beef", "Sausage"],
@@ -19,5 +23,28 @@ for (var ingredientType in ingredientsList) {
 	$("#pizzaIngredients").append(typeLabel).append(typeDiv); //Appends the type's div to the main page.
 }
 
-$('#menubar').menubar();  // Adding menu bar
-$('#navbar').navbar(); // Adding top navigation bar
+$('#menubar').menubar();		// Adding menu bar
+$('#navbar').navbar(); 			// Adding top navigation bar
+
+$('pizzaSizes').click(function() {
+	$(this).toggleClass('active');
+	console.log("hello");
+});
+
+smallPizza.click(function() {
+	smallPizza.toggleClass('active');
+	mediumPizza.removeClass('active');
+	largePizza.removeClass('active');
+});
+/*
+mediumPizza.click(function() {
+	smallPizza.css({ "fill" : "#ffffff", "stroke" : "#ffffff" });
+	mediumPizza.css({ "fill" : "#6b9b37", "stroke" : "#6b9b37"  });
+	largePizza.css({ "fill" : "#ffffff", "stroke" : "#ffffff" });
+});
+
+largePizza.click(function() {
+	smallPizza.css({ "fill" : "#ffffff", "stroke" : "#ffffff" });
+	mediumPizza.css({ "fill" : "#ffffff", "stroke" : "#ffffff" });
+	largePizza.css({ "fill" : "#6b9b37", "stroke" : "#6b9b37"  });
+});*/
