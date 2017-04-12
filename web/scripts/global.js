b993a2937f0d1a6dd2458734cae823e355e56a1a
Keyaku <antonio.luis.sarmento@gmail.com>
2017-04-12 19:11:52 +0100

    Global: Condensing these redefinitions

--- web/scripts/global.js
+++ web/scripts/global.js
@@ -1,12 +1,7 @@
 /******* (Re)defining Prototypes *******/
 // Array
-Array.prototype.first = function() {
-	return this[0];
-};
-
-Array.prototype.last = function() {
-	return this[this.length - 1];
-};
+Array.prototype.first = function() { return this[0]; };
+Array.prototype.last  = function() { return this[this.length - 1]; };
 
 /****** Code execution *******/
 $(document).ready(function(){
