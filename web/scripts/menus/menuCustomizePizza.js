1745481d613107253149fe91cbdc947c6ce9295b
Keyaku <antonio.luis.sarmento@gmail.com>
2017-04-12 18:41:24 +0100

    CustomizePizza: Simplified code for ingredients
    Changes:
    - Automated the application of ingredients images (no more checking the
    CSS file)
    - Added spaces to ingredient names
    - Added back Unicode characters to “Jalapeños”
    - Corrected spelling of “jalapeños” (I swear to God, I read “jalapenõs”
    one more Goddamn time and the one responsible will suffer BIGLY)
    - Removed manual
    - Swapped “ with ‘
    - Fixed some comments

--- web/scripts/menus/menuCustomizePizza.js
+++ web/scripts/menus/menuCustomizePizza.js
@@ -1,26 +1,8 @@
-var smallPizza = $('#size1');
-var mediumPizza = $('#size2');
-var largePizza = $('#size3');
-
-var ingredientsList = {
-	"Cheese"	: ["Mozzarella", "Parmesan", "ElementalCheese", "FrenchCheese", "GoatCheese", "Parmesan", "SourCream" ],
-	"Meat"		: ["Ham", "Pepperoni", "Bacon", "Prosciutto", "Chicken", "Beef", "Sausage"],
-	"Fish & Seafood" : ["Tuna", "Sardin"],
-	"Vegetables" : ["Mushroom", "Jalapenõs", "Tomato", "Peppers", "Arugula", "Basil", "Garlic", "GreenOlive", "Olives", "Onion", "Pineapple", "Spinach", "SweetCorn" ],
 };
 
-for (var ingredientType in ingredientsList) {
-	var typeLabel = $("<label></label>").addClass("pizzaIngredientTypeLabel").append(document.createTextNode(ingredientType)); //Creates the ingredient type label.
-	var typeDiv = $("<div></div>").addClass("pizzaIngredientType"); //Creates the div that contains the all the type's ingredients and their images.
-	for (i in ingredientsList[ingredientType]) {
-		var ingredientDiv = $("<div></div>").addClass("pizzaIngredient"); //Creates a div for each ingredient.
-		var ingredientLabel = $("<label></label>").addClass("pizzaIngredientName").append(document.createTextNode(ingredientsList[ingredientType][i])); //Creates the ingredient's label.
 
-		var str = "pizza" + ingredientsList[ingredientType][i];
-		ingredientDiv.append($("<div></div>").addClass("pizzaIngredientImg").attr("id", str)).append(ingredientLabel); //Appends the ingredient's image div and label to the ingredient's div.
 		typeDiv.append(ingredientDiv); //Appends the individual ingredients div to the types's div.
 	}
-	$("#pizzaIngredients").append(typeLabel).append(typeDiv); //Appends the type's div to the main page.
 }
 
 $('#menubar').menubar();		// Adding menu bar
