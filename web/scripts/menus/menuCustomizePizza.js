5ebf7d9e5bb57a96164937023fc88acf151d9b5d
Keyaku <antonio.luis.sarmento@gmail.com>
2017-04-12 18:43:41 +0100

    CustomizePizza: Completed previous commit
    GitHub Desktop, despite useful, can be a serious PITA.

--- web/scripts/menus/menuCustomizePizza.js
+++ web/scripts/menus/menuCustomizePizza.js
@@ -1,8 +1,33 @@
+const PATH_INGREDIENTS = 'img/menus/customizeMenu/pizzaIngredients/'; // Don't forget to consider our <base> rule.
+const LIST_INGREDIENTS = {
+	'Cheese'     : [
+		'Mozzarella', 'Parmesan', 'Elemental Cheese', 'French Cheese',
+		'Goat Cheese', 'Parmesan', 'SourCream'
+	],
+	'Meat'       : [
+		'Ham', 'Pepperoni', 'Bacon', 'Prosciutto', 'Chicken', 'Beef', 'Sausage'
+	],
+	'Fish'       : ['Tuna', 'Sardin'],
+	'Vegetables' : [
+		'Mushroom', 'Jalapeños', 'Tomato', 'Peppers', 'Arugula', 'Basil',
+		'Garlic', 'Green Olive', 'Olives', 'Onion', 'Pineapple', 'Spinach',
+		'Sweet Corn'
+	],
 };
 
+for (var ingredientType in LIST_INGREDIENTS) {
+	var typeLabel = $('<label></label>').addClass('pizzaIngredientTypeLabel').append(document.createTextNode(ingredientType)); // Creates the ingredient type label.
+	var typeDiv = $('<div></div>').addClass('pizzaIngredientType'); // Creates the div that contains the all the type's ingredients and their images.
+	for (i in LIST_INGREDIENTS[ingredientType]) {
+		var ingredientName = LIST_INGREDIENTS[ingredientType][i];
+		var ingredientDiv = $('<div></div>').addClass('pizzaIngredient'); // Creates a div for each ingredient.
+		var ingredientLabel = $('<label></label>').addClass('pizzaIngredientName').append(document.createTextNode(ingredientName)); // Creates the ingredient's label.
 
-		typeDiv.append(ingredientDiv); //Appends the individual ingredients div to the types's div.
+		ingredientName = ingredientName.toLowerCase().replace(/\s/g,'')
+		ingredientDiv.append($('<div></div>').addClass('pizzaIngredientImg').css({'background-image' : 'url("' + PATH_INGREDIENTS + ingredientName + '.png")'})).append(ingredientLabel); // Attaches the ingredient's image div and label to the ingredient's div.
+		typeDiv.append(ingredientDiv); // Appends the individual ingredients div to the types's div.
 	}
+	$('#pizzaIngredients').append(typeLabel).append(typeDiv); // Appends the type's div to the main page.
 }
 
 $('#menubar').menubar();		// Adding menu bar
