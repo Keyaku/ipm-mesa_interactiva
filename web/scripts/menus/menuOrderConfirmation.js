68335947a8b5310145dde36f08e319174864da8f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-04-19 22:24:06 +0100

    OrderConfirm: Various fixes
    Changes:
    - Swapped “ with ‘
    - Smaller IDs
    - Better jQuery

--- web/scripts/menus/menuOrderConfirmation.js
+++ web/scripts/menus/menuOrderConfirmation.js
@@ -3,15 +3,15 @@
 				CODE EXECUTION
 
 ------------------------------------------------------------------------------*/
-$("#menubar").menubar(); //Adding menu bar
-$("#navbar").navbar(); //Adding top navigation bar
+$('#menubar').menubar(); //Adding menu bar
+$('#navbar').navbar(); //Adding top navigation bar
 
-var index = sessionStorage.getItem("orderNumber"); //Gets the order number (in case the user is editing an order).
-var key = "order" + index.toString();
+var index = sessionStorage.getItem('orderNumber'); //Gets the order number (in case the user is editing an order).
+var key = 'order' + index.toString();
 var values = managerGetMetaValues(key);
-var orderedPizza = values[0];
-var orderedDrink = values[1];
-$("#order").append(createPizzaInfoWithSize(orderedPizza)).append(createDrinkItem(orderedDrink)); //Draws the order based on what was ordered previously.
+var orderedPizza = values[0],
+ 	orderedDrink = values[1];
+$('#order').append([createPizzaInfoWithSize(orderedPizza), createDrinkItem(orderedDrink)]); //Draws the order based on what was ordered previously.
 
 
 /*------------------------------------------------------------------------------
@@ -19,9 +19,10 @@ $("#order").append(createPizzaInfoWithSize(orderedPizza)).append(createDrinkItem
 				MENU FLOW
 
 ------------------------------------------------------------------------------*/
-$("#buttonAdd").click(function() { /*TODO - FranciscoKloganB: Create a popup to ask what the user wants to edit*/ }); //.
-$("#buttonConfirm").click(function() {
-	var i = sessionStorage.getItem("orders");
+$('button#add').click(function() { /*TODO - FranciscoKloganB: Create a popup to ask what the user wants to edit*/ }); //.
+$('button#confirm').click(function() {
+	var i = sessionStorage.getItem('orders');
 	var index = parseInt(i) + 1;
-	sessionStorage.setItem("orders", index.toString());
-	window.location.href = "html/table.html"; }); //Changes the page to the main page.
+	sessionStorage.setItem('orders', index.toString());
+	window.location.href = 'html/table.html';
+}); //Changes the page to the main page.
