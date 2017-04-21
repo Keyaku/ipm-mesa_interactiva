4f0e021bdf397c203f3f124932f615f6783f39ba
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-04-21 13:13:41 +0100

    Commented and refactored newOrder.js

--- web/scripts/newOrder.js
+++ web/scripts/newOrder.js
@@ -1,31 +1,30 @@
 /*------------------------------------------------------------------------------
 
-CODE EXECUTION
+			CODE EXECUTION
 
 ------------------------------------------------------------------------------*/
-$("#menubar").menubar();
-
-var orderNumber = parseInt(sessionStorage.getItem("orders"));
-for (var i = 0; i < orderNumber; i++) {
-	var str = "order" + i.toString();
-	var values = managerGetMetaValues(str);
-	if (values != null && values[0] != null && values[1] != null && (values[0] != ""  || values[1] != "")) {
-		createOrderItem(i);
-		shell(values[0], values[1], i);
+$('#menubar').menubar(); //Adds menu bar.
+//Shows the orders.
+for (var i = 0; i < sessionStorage.orders; i++) {
+	var str = 'order' + i.toString();
+	var values = managerGetMetaValues(str); //Gets the ordered pizza and drink.
+	if (values != null && values[0] != null && values[1] != null && (values[0] != '' || values[1] != '')) { //If the order wasn't deleted.
+		createOrderItem(i); //Creates the HTML structure for the order.
+		createOrderElements(values[0], values[1], i); //Fills the order item with the chosen pizza and drink.
 	}
 }
 
 /*
-$("#timer").countdown360({
+$('#timer').countdown360({
 	radius: 65,                        // radius of arc
-	strokeStyle: "#ecf0f1",            // the color of the stroke
+	strokeStyle: '#ecf0f1',            // the color of the stroke
 	strokeWidth: 5,					   // border radius
-	fillStyle: "#bdc3a7",              // the fill color
-	fontColor: "#ecf0f1",              // the font color
+	fillStyle: '#bdc3a7',              // the fill color
+	fontColor: '#ecf0f1',              // the font color
 	fontWeight: 700,                   // the font weight
 	autostart: true,                   // start the countdown automatically
 	seconds: 480,                      // the number of seconds to count down
-	label: ["second", "minute"],       // the label to use or false if none
+	label: ['second', 'minute'],       // the label to use or false if none
 	smooth: true,                      // should the timer be smooth or stepping
 	onComplete: function () {}
 }); //Adds the timer.
@@ -34,78 +33,78 @@ $("#timer").countdown360({
 
 /*------------------------------------------------------------------------------
 
-AUXILIAR FUNCTIONS
+			AUXILIAR FUNCTIONS
 
 ------------------------------------------------------------------------------*/
+//Creates the HTML structure for the order.
 function createOrderItem(index) {
 	var ind = index.toString();
-	var str = "order" + index.toString();
-	var div = $("<div></div>").addClass("orderStatusContainer").attr("id", str);
-	var a = $("<div></div>").addClass("col").attr("id", "pizza" + ind);
-	var b = $("<div></div>").addClass("col").attr("id", "drink" + ind);
-	var c = $("<div></div>").addClass("col").attr("id", "timer" + ind);
-	var d = $("<div></div>").addClass("col buttons");
-	var b1 = $("<button></button>").addClass("editcancel").addClass("buttonEdit").attr("id", "edit" + ind).append(document.createTextNode("Edit"));
-		var b11 = $("<button></button>").addClass("buttonEditPizza").attr("id", "editPizza" + ind).append(document.createTextNode("Pizza"));
-		var b12 = $("<button></button>").addClass("buttonEditDrink").attr("id", "editDrink" + ind).append(document.createTextNode("Drink"));
-	var b2 = $("<button></button>").addClass("editcancel").addClass("buttonCancel").attr("id", "cancel" + ind).append(document.createTextNode("Cancel"));
-	d.append(b1, b11, b12, b2);
-	div.append(a, b, c, d);
-	$("#orders").append(div);
+	var str = 'order' + index.toString();
+	var div = $('<div></div>').addClass('orderStatusContainer').attr('id', str); //Creates the div for the order.
+	var a = $('<div></div>').addClass('col').attr('id', 'pizza' + ind); //Creates the div for the ordered pizza.
+	var b = $('<div></div>').addClass('col').attr('id', 'drink' + ind); //Creates the div for the ordered drink.
+	//Creates the buttons.
+	var c = $('<div></div>').addClass('col buttons')
+			.append($('<button>', { html: 'Edit' }).addClass('editcancel buttonEdit').attr('id', 'edit' + ind))
+			.append($('<button>', { html: 'Pizza' }).addClass('buttonEditPizza').attr('id', 'editPizza' + ind))
+			.append($('<button>', { html: 'Drink' }).addClass('buttonEditDrink').attr('id', 'editDrink' + ind))
+			.append($('<button>', { html: 'Cancel' }).addClass('editcancel buttonCancel').attr('id', 'cancel' + ind));
+	div.append(a, b, c);
+	$('#orders').append(div);
 }
-function shell(pizza, drink, index) {
-	var orderedPizza = pizza; //Gets the ordered pizza.
-	var orderedDrink = drink; //Gets the ordered drink.
+//Fills the order item with the chosen pizza and drink.
+function createOrderElements(pizza, drink, index) {
 	setTimeout(function() {
-		$("#pizza" + index.toString()).append(createPizzaItemWithSize(orderedPizza)); //Shows the user's ordered pizza.
-		$("#drink" + index.toString()).append(createDrinkItem(orderedDrink)); //Shows the user's ordered drink.
+		$('#pizza' + index.toString()).append(createPizzaItemWithSize(pizza)); //Shows the ordered pizza.
+		$('#drink' + index.toString()).append(createDrinkItem(drink)); //Shows the ordered drink.
 	}, 100);
 }
 
+//When the client clicks the edit button.
 function orderEdit(index) {
-	editGetCategory(index); //Checks if the user wants to edit the pizza or the drink.
+	$('#editPizza' + index.toString()).toggle(); //Shows/hides the edit pizza button.
+	$('#editDrink' + index.toString()).toggle(); //Shows/hides the edit drink button.
 }
-function editGetCategory(index) {
-	$("#editPizza" + index.toString()).toggle();
-	$("#editDrink" + index.toString()).toggle();
+//When the client clicks the edit pizza button.
+function orderEditPizza(index) {
+	sessionStorage.setItem('orderNumber', index.toString()); //Sets the number of the current order.
+	sessionStorage.setItem('editing', 'true'); //Sets the editing flag to false.
+	window.location.href = 'html/menus/menuPizzaList.html';
 }
-function editPizza(index) {
-	sessionStorage.setItem("orderNumber", index.toString()); //Sets the number of the current order.
-	sessionStorage.setItem("editing", "true");
-	window.location.href = "html/menus/menuPizzaList.html";
+//When the client clicks the edit drink button.
+function orderEditDrink(index) {
+	sessionStorage.setItem('orderNumber', index.toString()); //Sets the number of the current order.
+	sessionStorage.setItem('editing', 'true'); //Sets the editing flag to false.
+	window.location.href = 'html/menus/menuDrinks.html';
 }
-function editDrink(index) {
-	sessionStorage.setItem("orderNumber", index.toString()); //Sets the number of the current order.
-	sessionStorage.setItem("editing", "true");
-	window.location.href = "html/menus/menuDrinks.html";
-}
-function cancelAllOrders() {
-	for (var order = 0; i < orderNumber; i++) {
-		orderCancel(order);
-	}
-}
-function orderCancel(index) {
-	confirmationOverlayShow(confirmCancel, [index]);
-}
+//When the client clicks the cancel button.
+function orderCancel(index) { confirmationOverlayShow(confirmCancel, [index]); }
+//When the client clicks the 'Yes' button in the confirmation overlay (callback from confirmationOverlayShow).
 function confirmCancel(args) {
 	var index = args[0];
-	$("#order" + index.toString()).remove();
+	$('#order' + index.toString()).remove();
 	managerDeleteOrder(index);
 }
+//When the client clicks the cancel all button.
+//TODO - @RafaelRibeiro
+function orderAllCancel() {  }
 
 
 /*------------------------------------------------------------------------------
 
-MENU FLOW
+			MENU FLOW
 
 ------------------------------------------------------------------------------*/
-$(".buttonEdit").click(function() {	editGetCategory(parseInt(($(this).attr("id"))[4])); }); //Edits the selected order.
-$(".buttonEditPizza").click(function() { editPizza(parseInt(($(this).attr("id"))[9])); }); //Edits the selected pizza.
-$(".buttonEditDrink").click(function() { editDrink(parseInt(($(this).attr("id"))[9])); }); //Edits the selected drink.
-
-$(".buttonCancel").click(function() { orderCancel(parseInt(($(this).attr("id"))[6])); }); //Cancells the selected order.
-$("#buttonNewOrder").click(function() {
-	var i = sessionStorage.getItem("orders"); //Gets the number of total orders.
-	sessionStorage.setItem("orderNumber", i); //Sets the number of the current order.
-	window.location.href = "html/menus/menuPizzaList.html";
-}); //Changes the page to the pizza menu.
+//The click event for the edit button shows the two buttons so the user chooses to edit the pizza or drink.
+$('.buttonEdit').click(function() {	orderEdit(parseInt(($(this).attr('id'))[4])); });
+//The click event for the edit pizza button edits the pizza.
+$('.buttonEditPizza').click(function() { orderEditPizza(parseInt(($(this).attr('id'))[9])); });
+//The click event for the edit drink button edits the drink.
+$('.buttonEditDrink').click(function() { orderEditDrink(parseInt(($(this).attr('id'))[9])); });
+//The click event for the cancel button cancels the order.
+$('.buttonCancel').click(function() { orderCancel(parseInt(($(this).attr('id'))[6])); }); //Cancells the selected order.
+//The click event for the new order buttons enables the client to order again.
+$('#buttonNewOrder').click(function() {
+	sessionStorage.setItem('orderNumber', sessionStorage.orders); //Sets the number of the current order.
+	window.location.href = 'html/menus/menuPizzaList.html'; //Continues with the ordering process.
+});
