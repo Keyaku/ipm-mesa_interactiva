e9e28e9077ec7fa6aaa742bfd63c56e52dcca359
Rafael Meneses Ribeiro <rafaelmmribeiro@gmail.com>
2017-04-30 15:41:33 +0100

    Added code for showing points of interest
    - Because of the API we're using, the workaround was to only be able to
    see on type of POI at a time.

--- web/scripts/informationMaps.js
+++ web/scripts/informationMaps.js
@@ -11,7 +11,6 @@ $('#mapsDestinationInput').hide();
 $('#go').hide();
 
 
-var transportationMode = '';
 //Default map variables
 var googleMapsKey = "?key=AIzaSyB1UiEgTrMu4oUPFCxorbwhTBMbX19RVGo";
 var googleMapsOrigin = "&origin=taguspark";
@@ -40,32 +39,50 @@ function directionsStart(button) {
 	$('#directionsButtonContainer').toggle();
 }
 
-function share() {
-	console.log("sharing");
-	confirmationOverlayShow(shared, []);
-}
+function share() { confirmationOverlayShow('Do you really wish to share your map?', shared, []); }
 function shared(arg) { }
-function interestsAdd(button) { button.toggleClass('active'); }
-function directionsChooseMode(button) {
+
+
+function mapGetPointsOfInterest(button) {
+	$('.interestsButton').removeClass('active');
+	button.addClass('active');
+	var pointType = button.attr('id');
+	var keyWords = '';
+	switch(pointType) {
+		case 'bars':
+			keyWords = 'bars+oeiras';
+			break;
+		case 'landscapes':
+			keyWords = 'parks+oeiras';
+			break;
+		case 'mall':
+			keyWords = 'malls+oeiras';
+			break;
+		case 'monuments':
+			keyWords = 'monuments+oeiras';
+			break;
+		case 'museums':
+			keyWords = 'museums+oeiras';
+			break;
+	}
+	var url = "https://www.google.com/maps/embed/v1/search"+googleMapsKey+"&q="+keyWords;
+	$("#iframeMap").attr("src", url);
+}
+
+function mapDirectionsChooseMode(button) {
 	$('.directionsButton').removeClass('active');
 	button.addClass('active');
 	$('#mapsDestinationInput').show();
 	$('#go').show();
-	transportationMode = (button.attr("id")).toLowerCase();
+	googleMapsMode = (button.attr("id")).toLowerCase();
 }
 function go() {
 	var destination = $('#mapsDestinationInput').val();
-	directionsMap(destination, transportationMode);
+	directionsMap(destination, googleMapsMode);
 }
 
-/*
-function searchMap(keyWords) {
-	var url = "https://www.google.com/maps/embed/v1/search"+googleMapsKey+"&q="+keyWords;
-	$("#iframeMap").attr("src", url);
-}
-*/
 function directionsMap(destination, travelMode) {
-	if (travelMode == '') { travelMode = googleMapsMode; }
+	if (travelMode == '') { travelMode = 'driving'; }
 	if (destination == '') {
 		var inputBar = $("#mapsDestinationInput");
 		inputBar.css("background-color", "red");
@@ -87,6 +104,6 @@ $('#mapsShare').click(function() { shareStart($(this)); });
 $('#mapsInterests').click(function() { interestsStart($(this)); });
 $('#mapsDirections').click(function() { directionsStart($(this)); });
 $('.shareButton').click(function() { share(); });
-$('.interestsButton').click(function() { interestsAdd($(this)); });
-$('.directionsButton').click(function() { directionsChooseMode($(this)); });
+$('.interestsButton').click(function() { mapGetPointsOfInterest($(this)); });
+$('.directionsButton').click(function() { mapDirectionsChooseMode($(this)); });
 $('#go').click(function() { go(); });
