5406a658759329a5df3626ae24023dd35ea12534
Keyaku <antonio.luis.sarmento@gmail.com>
2017-05-08 15:19:05 +0100

    Fixed tooltip error

--- web/scripts/global.js
+++ web/scripts/global.js
@@ -9,7 +9,6 @@ String.prototype.empty = function() { return !this.trim(); };
 $(document).ready(function(){
 	// Starting svg4everybody
 	svg4everybody({polyfill:true});
-	$('[data-toggle="tooltip"]').tooltip();
 });
 
 /****** Stuff *******/
