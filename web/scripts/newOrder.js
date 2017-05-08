e4c7912db070ab9a736b9fe93229719654c9dbe5
Keyaku <antonio.luis.sarmento@gmail.com>
2017-05-08 16:39:39 +0100

    More jQuery madness

--- web/scripts/newOrder.js
+++ web/scripts/newOrder.js
@@ -37,18 +37,27 @@ $('#timer').countdown360({
 ------------------------------------------------------------------------------*/
 //Creates the HTML structure for the order.
 function createOrderItem(index) {
-	var ind = index.toString();
-	var str = 'order' + index.toString();
-	var div = $('<div></div>').addClass('orderStatusContainer').attr('id', str); //Creates the div for the order.
-	var a = $('<div></div>').addClass('col').attr('id', 'pizza' + ind); //Creates the div for the ordered pizza.
-	var b = $('<div></div>').addClass('col').attr('id', 'drink' + ind); //Creates the div for the ordered drink.
+	var ind = index;
+	var str = 'order' + index;
+	var div = $('<div>', {
+		'class': 'orderStatusContainer',
+		'id': str
+	}); //Creates the div for the order.
+	var a = $('<div>', {
+		'class': 'col',
+		'id': 'pizza' + ind
+	}); //Creates the div for the ordered pizza.
+	var b = $('<div>', {
+		'class': 'col',
+		'id': 'drink' + ind
+	}); //Creates the div for the ordered drink.
 	//Creates the buttons.
-	var c = $('<div></div>').addClass('col buttons')
+	var c = $('<div>', { 'class': 'col buttons' })
 			.append($('<button>', { html: 'Edit' }).addClass('buttonEdit buttonNeutral').attr('id', 'edit' + ind))
 			.append($('<button>', { html: 'Pizza' }).addClass('buttonEditPizza buttonNeutral').attr('id', 'editPizza' + ind))
 			.append($('<button>', { html: 'Drink' }).addClass('buttonEditDrink buttonNeutral').attr('id', 'editDrink' + ind))
 			.append($('<button>', { html: 'Cancel' }).addClass('buttonCancel buttonDanger').attr('id', 'cancel' + ind));
-	div.append(a, b, c);
+	div.append([a, b, c]);
 	$('#orders').append(div);
 }
 //Fills the order item with the chosen pizza and drink.
@@ -57,26 +66,26 @@ function createOrderElements(pizza, drink, index) {
 	var drinkNumber = Number(drink['drinkNumber']);
 	var incButtons = createIncrementButtons(pizzaNumber, drinkNumber);
 	setTimeout(function() {
-		if (pizza != '') { $('#pizza' + index.toString()).append(createPizzaItemWithSize(pizza), incButtons[0]); }//Shows the ordered pizza.
-		if (drink != '') { $('#drink' + index.toString()).append(createDrinkItem(drink), incButtons[1]); }//Shows the ordered drink.
+		if (pizza != '') { $('#pizza' + index).append(createPizzaItemWithSize(pizza), incButtons[0]); }//Shows the ordered pizza.
+		if (drink != '') { $('#drink' + index).append(createDrinkItem(drink), incButtons[1]); }//Shows the ordered drink.
 	}, 100);
 }
 
 //When the client clicks the edit button.
 function orderEdit(index) {
-	$('#editPizza' + index.toString()).toggle(); //Shows/hides the edit pizza button.
-	$('#editDrink' + index.toString()).toggle(); //Shows/hides the edit drink button.
+	$('#editPizza' + index).toggle(); //Shows/hides the edit pizza button.
+	$('#editDrink' + index).toggle(); //Shows/hides the edit drink button.
 }
 //When the client clicks the edit pizza button.
 function orderEditPizza(index) {
-	sessionStorage.setItem('orderNumber', index.toString()); //Sets the number of the current order.
-	sessionStorage.setItem('editing', 'true'); //Sets the editing flag to false.
+	sessionStorage.orderNumber = index; //Sets the number of the current order.
+	sessionStorage.editing = true; //Sets the editing flag to false.
 	window.location.href = 'html/menus/menuPizzaList.html';
 }
 //When the client clicks the edit drink button.
 function orderEditDrink(index) {
-	sessionStorage.setItem('orderNumber', index.toString()); //Sets the number of the current order.
-	sessionStorage.setItem('editing', 'true'); //Sets the editing flag to false.
+	sessionStorage.orderNumber = index; //Sets the number of the current order.
+	sessionStorage.editing = true; //Sets the editing flag to false.
 	window.location.href = 'html/menus/menuDrinks.html';
 }
 
@@ -134,7 +143,7 @@ function orderCancel(index) { confirmationOverlayShow('Do you really wish to can
 //When the client clicks the 'Yes' button in the confirmation overlay (callback from confirmationOverlayShow).
 function confirmCancel(args) {
 	var index = args[0];
-	$('#order' + index.toString()).remove();
+	$('#order' + index).remove();
 	managerDeleteOrder(index);
 }
 //When the client clicks the cancel all button.
@@ -163,6 +172,6 @@ $('.buttonCancel').click(function() { orderCancel(parseInt(($(this).attr('id'))[
 $('#buttonCancelAll').click(function() { orderAllCancel(); }); //Cancells the selected order.
 //The click event for the new order buttons enables the client to order again.
 $('#buttonNewOrder').click(function() {
-	sessionStorage.setItem('orderNumber', sessionStorage.orders); //Sets the number of the current order.
+	sessionStorage.orderNumber = sessionStorage.orders; //Sets the number of the current order.
 	window.location.href = 'html/menus/menuPizzaList.html'; //Continues with the ordering process.
 });
