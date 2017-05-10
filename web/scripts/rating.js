31b9adbe2ac48c5da7e5d61d923fd9072061873a
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-05-10 14:39:00 +0000

    Rating starting point

--- web/scripts/rating.js
+++ web/scripts/rating.js
@@ -1,22 +1,30 @@
 /*------------------------------------------------------------------------------
 
-				MODAL MANAGEMENT
-
-------------------------------------------------------------------------------*/
-
-function execute(args) { window.location.href = 'html/table.html'; }
-$('#buttonCancel').click(function() { confirmationOverlayShow("Do you really wish to skip the rating step?", execute, []); });
-
-/*------------------------------------------------------------------------------
-
 				STAR RATING
 
 ------------------------------------------------------------------------------*/
 
-//Creates a div for the rating stars and the rating label.
-function createStarContainer() {
-	var d = $('<div>').addClass('starContainer');
-	for (var i = 1; i <= starNum; i++) { d.append($('<i>').addClass('fa fa-star').addClass('coloredStar')); }
-	for (var i = starNum + 1; i <= 5; i++) { d.append($('<i>').addClass('fa fa-star')); }
-	return d;
+//Appends 5 stars to starContainer div.
+function createRating() {
+	var maxRating = 5;
+	var sC = $('#starContainer');
+	for (var i = 1; i <= maxRating; i++) {
+		sC.append($('<i>').addClass('fa fa-star'));
+	}
 }
+
+$('window').load(createRating());
+
+/*------------------------------------------------------------------------------
+
+				MODAL MANAGEMENT
+
+------------------------------------------------------------------------------*/
+
+function execute(args) {
+	window.location.href = 'html/table.html';
+}
+
+$('#buttonCancel').click(function() {
+	confirmationOverlayShow("Do you really wish to skip the rating step?", execute, []);
+});
