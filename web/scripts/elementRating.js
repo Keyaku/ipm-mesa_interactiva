e55f33c20bded30e0b3dc0960cccfaf3e21111c1
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-05-12 00:25:40 +0000

    elementRating - Not working

--- web/scripts/elementRating.js
+++ web/scripts/elementRating.js
@@ -1,10 +1,9 @@
-const MAX_RATING = 5;
-
 /*******************************************************************************
 
 	BASE SCRIPTS - NAVBAR
 
 *******************************************************************************/
+const MAX_RATING = 5;
 
 $(document).ready(function() {
 	$('#buttonConfirm').hide();
@@ -50,12 +49,42 @@ function createOrderElements(pizza, drink, index) {
 }
 
 function createRating(index) {
+	var starIndex;
 	var orderIndex = $('#order' + index + 'rating');
-	var starIndex = 'order' + index + 'rating' + 'star' + index;
 	for (var i = 1; i <= MAX_RATING; i++) {
+		starIndex = 'order' + index + 'ratingStar' + i;
 		orderIndex.append($('<i>').addClass('fa fa-star').attr('id', starIndex));
 	}
 }
+
+/**
+* Se o locator funcionar ele vai retornar posicoes de 0 ate N-1 pizzas encomendadas
+* Como os nossos ciclos vao da estrela 1 ate 5 inclusive, entao temos de somar 1.
+* O a multiplicacao por 5 permite identificar sobre que conjunto de estrelas vamos atuar.
+* Se o locator retornar 0 entao 0 * 5 = 0, pelo que vamos actuar na primeira pizza da lista.
+* Se for 1, entao vamos atuar na segunda pizza, etc.
+* O resto funciona como em rating.js
+* Problema obter o valor de locator...
+*/
+$('.orderStatusContainer').on("click", ".fa.fa-star", function() {
+	var locator = $('.orderStatusContainer').index(this);
+	var startPoint = 1 + (locator * 5);
+	var endPoint = startPoint + MAX_RATING;
+	var currentStar;
+	alert(locator);
+	var clickedIndex = $('.fa').index(this);
+	alert(clickedIndex);
+	$('#buttonConfirm').show();
+	for (var i = startPoint; i <= endPoint; i++) {
+		currentStar = $('#order' + locator + 'ratingStar' + i);
+		if (i <= clickedIndex) {
+			currentStar.css({"color" : "green"});
+		} else {
+			currentStar.css({"color" : "white"});
+		}
+	}
+});
+
 /*******************************************************************************
 
 	MODAL MANAGEMENT
