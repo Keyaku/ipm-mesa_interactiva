abb761fef70ea77e949a0a6d8e99e9d05926a3b8
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-05-14 11:43:26 +0000

    Pizza rating: closer to solution

--- web/scripts/elementRating.js
+++ web/scripts/elementRating.js
@@ -57,27 +57,22 @@ function createRating(index) {
 	}
 }
 
-/**
-* Se o locator funcionar ele vai retornar posicoes de 0 ate N-1 pizzas encomendadas
-* Como os nossos ciclos vao da estrela 1 ate 5 inclusive, entao temos de somar 1.
-* O a multiplicacao por 5 permite identificar sobre que conjunto de estrelas vamos atuar.
-* Se o locator retornar 0 entao 0 * 5 = 0, pelo que vamos actuar na primeira pizza da lista.
-* Se for 1, entao vamos atuar na segunda pizza, etc.
-* O resto funciona como em rating.js
-* Problema obter o valor de locator...
-*/
 $('.orderStatusContainer').on("click", ".fa.fa-star", function() {
-	var locator = $('.orderStatusContainer').index(this);
-	var startPoint = 1 + (locator * 5);
-	var endPoint = startPoint + MAX_RATING;
+	var clickedStar = $('.fa').index(this);
+	alert('clickedStar: ' + clickedStar);
+	var orderNumber = Math.floor(clickedStar / 5);
+	alert('orderNumber: '+ orderNumber);
+	var starsToColor = clickedStar % 5;
+	alert('starsToColor:' + starsToColor);
+	var startPoint = 1 + (clickedStar - starsToColor);
+	alert('startPoint: ' + startPoint);
+	var endPoint = clickedStar + (MAX_RATING - starsToColor);
+	alert('endPoint: ' + endPoint);
 	var currentStar;
-	alert(locator);
-	var clickedIndex = $('.fa').index(this);
-	alert(clickedIndex);
 	$('#buttonConfirm').show();
 	for (var i = startPoint; i <= endPoint; i++) {
-		currentStar = $('#order' + locator + 'ratingStar' + i);
-		if (i <= clickedIndex) {
+		currentStar = $('#order' + orderNumber + 'ratingStar' + i);
+		if (i <= clickedStar) {
 			currentStar.css({"color" : "green"});
 		} else {
 			currentStar.css({"color" : "white"});
